afe74e65d243820b006e188c523a5d33a8e69b14
Team @MoviZenX <84813706+saikatsingh@users.noreply.github.com>
2021-08-28 21:49:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,10 @@ import os
 
 
 class Config(object):
-      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-      API_ID = int(os.environ.get("APP_ID", 12345))
-      API_HASH = os.environ.get("API_HASH")
-      CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "")
-      CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "nil")
+      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "1875358697:AAFMnsc-LOCl_GFnduwFQprQTnZNmVXF4a4")
+      API_ID = int(os.environ.get("5704620")
+      API_HASH = os.environ.get("1830a03619ac72c0dd69fb81c1d3f2c4")
+      CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "**ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ**: **@MoviZenX**
+**ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ**: **@MVZNX**")
+      CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "Bottom")
       ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "AnnihilusOP")
